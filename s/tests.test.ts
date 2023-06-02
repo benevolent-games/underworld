dd7a1d45b76a72251e3f0b00017a1f5063e1c840
Chase Moskal <chasemoskal@gmail.com>
2023-06-02 12:08:23 -0700

    tweak: use tiny dungeon for test

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -4,14 +4,14 @@ import {cardinal} from "./tools/cardinal.js"
 import {generate_dungeon} from "./pathing/generate_dungeon.js"
 import {make_text_view_for_dungeon} from "./text/make_text_view_for_dungeon.js"
 
-const random = Randy.seed(61)
+const random = Randy.seed(1)
 const randy = new Randy(random)
 
 const start_time = performance.now()
 
 const tiles = generate_dungeon({
 	randy,
-	number_of_tiles_between_start_and_end: 11,
+	number_of_tiles_between_start_and_end: 3,
 	number_of_big_tiles: 1, // Math.round(randy.between(1, 2)),
 	chance_of_grid_sprout: 7 / 10,
 	chance_of_sprout_attempting_to_loop: 7 / 10,
